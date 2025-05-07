04eb32932ff724634abeab36cc627934bbb81724
zhaiiyq <akzajykp@gmail.com>
2025-05-07 17:33:41 +0500

    Update games_recommentadiot_streamlit.py

--- games_recommentadiot_streamlit.py
+++ games_recommentadiot_streamlit.py
@@ -7,7 +7,7 @@ st.markdown("""
     <style>
     html, body, .stApp {
         background-color: #0d0d1a;
-        background-image: url('https://imgur.com/DmP7IJq');
+        background-image: url('https://imgur.com/DmP7IJq.gif');
         background-size: cover;
         background-position: center;
         background-attachment: fixed;
